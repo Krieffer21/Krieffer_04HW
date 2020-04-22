19e2c9213438cdcb243881da67de95500ee3ca90
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-04-22 12:05:17 -0700

    adding questions and answers as variables

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 var timer = document.querySelector("#timer");
-timer.textContent= "Timer: 100"; 
+timer.textContent = "Timer: 100"; 
 var highScore = document.querySelector(".score");
 var body = document.querySelector("body");
-var q1 = "ksdlvjbndlfj";
-var a1 = ["jefnafj","jfdsbk","fdjas","sfjdkbv"];
+    var q1 = "Which of the following data types represents an object?" ;
+    var a1 = ["var x = 21;","var x = \'Billy\'","var x = \'Billy Bob\'","var x = {name:Billy};"];
+    var q2 = "How would you call the following function. function example() { Alert (\“Pick me!\”); }";
+    var a2 = ["(Pick Me!)", "Alert (Pick me!);", "example();", "Return function ();"];
+    var q3 = "Which of the following is an example of an array?"
+    var a3 = ["var cat = [\“cute\”, \“furry\”, \“sleepy\”];", "var cat = true;", "var cat = (cute, furry, sleepy);", "var cat = \“cute\”, \“furry\”, \“sleepy\”;"];
+    var q4 = "Math.random(); is used for which of the following?";
+    var a4 = ["Does math", "Returns a random number", "Nothing", "Makes me happy"];
+    var q5 = "A variable declared in the overall script outside of any function is?";
+    var a5 = ["A Lazy variable", "A Local variable", "A Useless variable", "A Global variable"];
 
-var header= document.createElement("header");
-header.textContent= "Coding Quiz!";
+var header = document.createElement("header");
+header.textContent = "Coding Quiz!";
 body.appendChild(header);
 
-var intro= document.createElement("p");
-intro.textContent= "Please answer the following code related questions before time runs out! Remeber for every correct answer you will gain a point, but for every incorrect answer you will lose a point.";
+var intro = document.createElement("p");
+intro.textContent = "Please answer the following code related questions before time runs out! Remeber for every correct answer you will gain a point, but for every incorrect answer you will lose a point.";
 body.appendChild(intro);
 
 var start = document.querySelector ("button");
-start.textContent= "Start Quiz";
+start.textContent = "Start Quiz";
 body.appendChild(start);
 
 start.addEventListener("click", function() {
@@ -43,22 +51,22 @@ var answer4;
 
 function askQuestion(q,a) {
     if (question == undefined){
-        question= document.createElement("h1");
+        question = document.createElement("h1");
         body.appendChild(question); 
-        answer1= document.createElement("button");
+        answer1 = document.createElement("button");
         body.appendChild(answer1);
-        answer2= document.createElement("button");
+        answer2 = document.createElement("button");
         body.appendChild(answer2);
-        answer3= document.createElement("button");
+        answer3 = document.createElement("button");
         body.appendChild(answer3);
-        answer4= document.createElement("button");
+        answer4 = document.createElement("button");
         body.appendChild(answer4);   
     }
-    question.textContent= q;
-    answer1.textContent= a[0];
-    answer2.textContent= a[1];
-    answer3.textContent= a[2];
-    answer4.textContent= a[3];
+    question.textContent = q;
+    answer1.textContent = a[0];
+    answer2.textContent = a[1];
+    answer3.textContent = a[2];
+    answer4.textContent = a[3];
 }
 
 
